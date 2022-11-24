098b3e8907145b5f9c5c64ae829d8e7164550ed0
serhii1976 <zhiganov76@gmail.com>
2022-11-24 15:39:15 +0000

    Deploying to gh-pages from @ serhii1976/goit-react-hw-08-phonebook@754ef6488bba4c3afd813e929f0eb86cc81c543a 🚀

---
+++ static/js/266.82b81ede.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var c=n(184),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function r(){return(0,c.jsx)("div",{style:i.container,children:(0,c.jsx)("h1",{style:i.title,children:"Contacts book welcome page"})})}}}]);
+//# sourceMappingURL=266.82b81ede.chunk.js.map
